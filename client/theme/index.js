2e2a5ced77466e60a99dfa67d8352dc96198a701
HACXOR298 <144638103+HACXOR298@users.noreply.github.com>
2023-10-13 18:04:35 +0500

    Update index.js

--- client/theme/index.js
+++ client/theme/index.js
@@ -25,7 +25,15 @@ const pallete = [
         // red
         text: '#dc2626',
         bgColor: opacity=> `rgba(248, 113, 113, ${opacity})`
+    },
+    {
+
+//pink 
+          text: '#dc2626',
+        bgColor: opacity=> `rgba(248, 113, 113, ${opacity})`
+        
     }
+    
 
 ]
 export const themeColors = {
